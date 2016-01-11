96fb5e01c3b7d0139b894208110f28ca18ba90db
chikilov <cm@anticgame.co.kr>
2016-01-11 18:38:40 +0900

    remove old admin

--- application/views/include/subnavi_rankprev.php
+++
@@ -1,7 +0,0 @@
-<div class="section">
-			<h2>랭킹 - 지난 랭킹</h2>
-			<ul class="subNav">
-				<li><a href="<?php echo URLBASE; ?>index.php/pages/admin/rankprevweek/view/0">1vs1</a></li>
-				<li><a href="<?php echo URLBASE; ?>index.php/pages/admin/rankprevweek/view/1">보스대전</a></li>
-				<li><a href="<?php echo URLBASE; ?>index.php/pages/admin/rankprevweek/view/2">생존모드</a></li>
-			</ul>
--- application/views/webview/include/db_connect.php
+++
@@ -1,4 +0,0 @@
-<?php	
-	//$connect = mysqli_connect('1.234.6.103','root','dudrud0612@') or die("Error " . mysqli_error($connect));
-	$connect = mysqli_connect('172.19.172.69','webuser','dudrud0612@') or die("Error " . mysqli_error($connect));	
-?>
